20764e579719138ea68027de6119faeabc05f7e3
SravaniBommidi <104830120+SravaniBommidi@users.noreply.github.com>
2022-05-08 11:00:24 +0530

    Update HelloWorldExample.java

--- src/main/java/com/example/HelloWorldExample.java
+++ src/main/java/com/example/HelloWorldExample.java
@@ -9,6 +9,7 @@ public class HelloWorldExample extends HttpServlet
   public void service( HttpServletRequest req, HttpServletResponse res ) throws IOException {
     PrintWriter out = res.getWriter();
     out.println( "Hello, World!" );
+    out.println( "Hi All" );
     out.close();
   }
 }
